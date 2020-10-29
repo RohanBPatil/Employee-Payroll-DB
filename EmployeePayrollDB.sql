7e8aa23e50fdb42a374279427c1f485994511072
RohanBPatil <rohanpatil14218@gmail.com>
2020-10-30 00:57:13 +0530

    Added : Functionality to insert data into table

--- EmployeePayrollDB.sql
+++ EmployeePayrollDB.sql
@@ -4,7 +4,7 @@ show databases;		#To see all databases
 create database payroll_service;	# to create database
 use payroll_service;			# to use payroll_service database
 
-#uc2
+#UC 2
 create table employee_payroll		# to create table
 (
 id int not null auto_increment,
@@ -14,3 +14,10 @@ start date not null,
 primary key (id)
 );
 describe employee_payroll;
+
+#UC 3
+insert into employee_payroll (name, salary, start)
+values
+('Shahrukh', 100000, '2018-01-03'),
+('Anil', 300000, '2019-11-13'),
+('Makrand', 500000, '2020-05-21');
