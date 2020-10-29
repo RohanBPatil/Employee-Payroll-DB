c44ed056235b9d2f7de011abef128730e5496234
RohanBPatil <rohanpatil14218@gmail.com>
2020-10-30 00:45:09 +0530

    Added : Functionality to create table

--- EmployeePayrollDB.sql
+++ EmployeePayrollDB.sql
@@ -1,5 +1,16 @@
 show databases;		#To see all databases
 
 #UC 1
-create database payroll_service;	# create database
+create database payroll_service;	# to create database
 use payroll_service;			# to use payroll_service database
+
+#uc2
+create table employee_payroll		# to create table
+(
+id int not null auto_increment,
+name varchar(100) not null,
+salary double not null,
+start date not null,
+primary key (id)
+);
+describe employee_payroll;
