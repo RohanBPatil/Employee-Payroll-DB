25ae98a63dda64b31d363fcb7feedf9d0a84caae
RohanBPatil <rohanpatil14218@gmail.com>
2020-10-31 01:16:59 +0530

    Added : Ability to extend employee payroll table to have different pays

--- EmployeePayrollDB.sql
+++ EmployeePayrollDB.sql
@@ -55,3 +55,10 @@ alter table employee_payroll
 add phone numeric(10) not null after name,
 add address varchar(200) not null default 'Mumbai' after phone,
 add department varchar(200) not null after address;
+
+#UC 9
+alter table employee_payroll rename column salary to basic_pay, 
+add deductions double not null after basic_pay, 
+add taxable_pay double not null after deductions, 
+add tax double not null after taxable_pay, 
+add net_pay double not null after tax;
